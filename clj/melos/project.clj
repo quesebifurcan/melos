136d6316c54a2d6ab7189ebe3b63fab34f9d79a9
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-05-19 16:11:04 +0200

    Sort lines in project.clj.

--- clj/melos/project.clj
+++ clj/melos/project.clj
@@ -3,9 +3,9 @@
                  [org.clojure/algo.generic "0.1.2"]
                  [org.clojure/data.json "0.2.5"]
                  [org.clojure/math.combinatorics "0.0.8"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/tools.trace "0.7.8"]
                  [prismatic/plumbing "0.4.2"]
-                 [prismatic/schema "0.4.0"]
+                 [prismatic/schema "0.4.0"]]
   :plugins [[lein-marginalia "0.8.0"]])
 
