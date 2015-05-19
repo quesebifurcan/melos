dd808ff028f45cb89a44aa9fbbb875de25dfbf3c
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-05-19 15:36:52 +0200

    Update project file with dependencies.

--- clj/melos/project.clj
+++ clj/melos/project.clj
@@ -1,6 +1,11 @@
 (defproject melos "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/math.combinatorics "0.0.8"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+                 [org.clojure/tools.trace "0.7.8"]
+                 [prismatic/plumbing "0.4.2"]
+                 [prismatic/schema "0.4.0"]
+  :plugins [[lein-marginalia "0.8.0"]])
+
--- clj/melos/src/melos/core.clj
+++ clj/melos/src/melos/core.clj
@@ -1,6 +1,1 @@
 (ns melos.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
