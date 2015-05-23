2dbb41ea7a2c26d453d5eb8530a76e4af050f709
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-05-23 11:09:22 +0200

    First version of melodic contour -- prefer small intervals, avoid repeated unisons, allow for center-pitch to move around.

--- clj/melos/src/melos/tools/contour.clj
+++ clj/melos/src/melos/tools/contour.clj
@@ -17,6 +17,67 @@
 
 (get-options 62 3)
 
+(defn score-distance
+  [pitches]
+  (let [distance (reduce + 0
+                             (map (fn [x] (Math/abs (- 60 x)))
+                      pitches))]
+    distance))
+
+(defn remove-unisons
+  [step]
+  (if (= step 0) 100 step))
+
+(defn score-interval-sizes
+  [pitches]
+  (let [groups (partition 2 1 pitches)]
+    (->> (map (fn [[x y]] (Math/abs (- x y))) groups)
+         (map remove-unisons)
+         (reduce + 0))))
+
+(defn normalize
+  [x min- max-]
+  (map (fn [y] (/ (- y min-)
+                  (- max- min-)))
+       x))
+
+(defn score-segment
+  [group]
+  (let [distances (map score-distance group)
+        distance-min (apply min distances)
+        distance-max (apply max distances)
+        distances (normalize distances
+                             distance-min distance-max)
+        intervals (map score-interval-sizes group)
+        intervals-min (apply min intervals)
+        intervals-max (apply max intervals)
+        intervals (normalize intervals
+                             intervals-min intervals-max)]
+        
+    (map (fn [pitches distance interval]
+           {:pitches pitches
+            :distance distance
+            :interval interval})
+         group
+         distances
+         intervals)))
+
+(defn sort-scores
+  [scores]
+  (sort-by (fn [{:keys [distance interval]}]
+                (+ distance interval))
+           scores))
+
+(let [mel [0 10 8 7 2 2 2 2 4 3]]
+  (->> (map (partial get-options 60) mel)
+       (apply combinatorics/cartesian-product)
+       ;; (map (fn [x]
+       ;;    (do (println x "\n")
+       ;;        x)))
+       (score-segment) 
+       (sort-scores)
+       ))
+
 ;; pass in earlier stages to improve result
 ;; (let [a [[0] [987] [1 2] [3 4] [5 6]]]
 ;;   (apply combinatorics/cartesian-product a))
