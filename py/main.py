f2d9651ad7db582312b7d86037ba7cf0118c977c
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-05 02:07:06 +0200

    Improve layout.

--- py/main.py
+++ py/main.py
@@ -8,9 +8,22 @@ def make_lilypond_file(score):
     lilypond_file = lilypondfiletools.make_basic_lilypond_file(score)
     lilypond_file.default_paper_size = 'a4', 'portrait'
     lilypond_file.global_staff_size = 14
-    lilypond_file.layout_block.indent = 0
+    # lilypond_file.layout_block.indent = 10
     # lilypond_file.layout_block.ragged_right = True
     lilypond_file.paper_block.ragged_bottom = True
+    lilypond_file.header_block.composer = Markup('Fredrik Wallberg')
+    lilypond_file.header_block.title = Markup('Map')
+    # lilypond_file.paper_block.top_margin = 20
+    # lilypond_file.paper_block.top_margin_spacing = 100
+    lilypond_file.layout_block.left_margin = 10
+    spacing_vector = layouttools.make_spacing_vector(0, 0, 8, 0)
+    lilypond_file.paper_block.system_system_spacing = spacing_vector
+    spacing_vector = layouttools.make_spacing_vector(0, 0, 20, 0)
+    lilypond_file.paper_block.top_markup_spacing = spacing_vector
+    spacing_vector = layouttools.make_spacing_vector(0, 0, 1, 0)
+    override(score).staff_grouper.staff_staff_spacing = spacing_vector
+    spacing_vector = layouttools.make_spacing_vector(0, 0, 3, 0)
+    override(score).vertical_axis_group.staff_staff_spacing = spacing_vector
     return lilypond_file
 
 def is_tuplet(d):
@@ -90,6 +103,20 @@ def main():
         }
 
     attach(Clef('bass'), staves.get('ped'))
+    upper, lower, ped = [staves.get(x) for x in ['upper', 'lower', 'ped']]
+    group = scoretools.StaffGroup([upper, lower])
+    man_instr = instrumenttools.Harp(instrument_name=r'Manuals',
+                                       short_instrument_name='Man.')
+    ped_instr = instrumenttools.Instrument(instrument_name=r'Pedals',
+                                       short_instrument_name='Ped.')
+    group.context_name = 'PianoStaff'
+    # organ_instr = instrumenttools.Harp()
+    attach(man_instr, group)
+    attach(ped_instr, ped)
+
+    f(group)
+    # import sys; sys.exit()
+    # staves.get('upper').consists_commands.append('Horizontal_bracket_engraver')
 
     parts = staves.keys()
 
@@ -122,7 +149,17 @@ def main():
                 current_clef = clef
 
     score = Score(
-        [staves.get(part) for part in 'upper lower ped'.split()])
+        [group, staves.get('ped')])
+    set_(score).tuplet_full_length = True
+    # override(score).tuplet_bracket.padding = 2
+    # override(score).tuplet_bracket.staff_padding = 4
+    scheme = schemetools.Scheme('tuplet-number::calc-fraction-text')
+    override(score).tuplet_number.text = scheme
+    override(score).tuplet_bracket.direction = 'up'
+    spacing_vector = layouttools.make_spacing_vector(0, 0, 30, 0)
+    # override(score).vertical_axis_group.staff_staff_spacing = spacing_vector
+    # override(score).staff_grouper.staff_staff_spacing = spacing_vector
+
     lilypond_file = make_lilypond_file(score)
 
     show(lilypond_file)
