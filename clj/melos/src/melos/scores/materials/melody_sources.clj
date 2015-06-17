022523b4cdc7c7edafdfd5e752684cab5a1013d4
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-17 08:30:15 +0200

    Move functions used by all generators in scores.tools.

--- clj/melos/src/melos/scores/materials/melody_sources.clj
+++ clj/melos/src/melos/scores/materials/melody_sources.clj
@@ -2,15 +2,14 @@
   (:require [melos.scores.materials.event-seqs :refer [pendulum-1
                                                        pendulum-2
                                                        lindenmayer-1]]
-            [melos.scores.event-seqs.upper :refer [
-                                                    upper-soft
-                                                    unfold-events]]))
+            [melos.scores.tools :as tools]
+            [melos.scores.event-seqs.upper :refer [upper-soft]]))
 
 (defn organ
   []
   ;; TODO: return lazy seqs.
   ;; {:upper {:a (pendulum-1 :upper 3)}
-  {:upper {:a (unfold-events (upper-soft) :upper)}
+  {:upper {:a (tools/unfold-events (upper-soft) :upper)}
   ;; {:upper {:a (tes/unfold-events (tes/morph))}
    :lower {:a (pendulum-2 :lower)}
    :ped {:a (lindenmayer-1 :ped 22)
--- clj/melos/src/melos/scores/tools.clj
+++ clj/melos/src/melos/scores/tools.clj
@@ -1,8 +1,52 @@
 (ns melos.scores.tools
   (:require [schema.core :as s]
             [melos.tools.rtm :as rtm]
-            [melos.tools.utils :refer [merge-in]]
+            [melos.tools.make-note :refer [make-note]]
+            [melos.tools.utils :refer [merge-in
+                                       rotate]]
             [melos.tools.schemata :as schemata]))
+
+
+(defn make-chord-from-pitch-vector-params
+  [{:keys [pitch] :as m}]
+  (let [group (gensym "G__")]
+  (map (fn [p]
+         (make-note (merge {:pitch p :group group}
+                           (dissoc m :pitch))))
+       pitch)))
+
+(defn parse-params
+  [x]
+  (if (list? x)
+    (repeat (last x) (first x))
+    [x]))
+
+(defn unfold-events
+  [m part]
+  (let [m (assoc m :part [part])
+        f (:fn m)
+        partition-fn (:partition m)
+        m (dissoc m :fn :partition)]
+  (->> (map (fn [x] (mapcat parse-params x))
+            (vals m))
+       (map cycle)
+       (apply map vector)
+       (map (fn [x] (zipmap (keys m) x)))
+       (map f)
+       (partition-fn))))
+
+(defn cyclic-partition
+  [xs splits]
+  (cons (take (first splits) xs)
+        (lazy-seq (cyclic-partition (drop (first splits) xs)
+                                    (rotate splits)))))
+
+(defn cyclic-repeats
+  [xs repeats]
+  (if (seq xs)
+    (concat (repeat (first repeats) (first xs))
+            (lazy-seq (cyclic-repeats (rotate xs)
+                                      (rotate repeats))))))
 
 (s/defn unfold-segments
   :- [schemata/ScoreSegment]
