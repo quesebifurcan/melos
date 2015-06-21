024d378021561567e77f7cc2b48b0108e0759622
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-21 22:48:20 +0200

    New helper function for combining several partitions. Not the most elegant, but it works for now.

--- clj/melos/src/melos/scores/event_seqs/upper.clj
+++ clj/melos/src/melos/scores/event_seqs/upper.clj
@@ -2,6 +2,43 @@
   (:require [melos.scores.tools :as tools]
             [melos.scores.utils :as utils]))
 
+(def pitches
+  (let [ranges [
+                (range 0 10)
+                (range 3 10)
+                (range 0 7)
+                ]
+        partitions (map count ranges)
+    ;; (->> (mapcat (fn [x]
+    ;;                (take-while (complement empty?)
+    ;;                            (tools/cyclic-partition
+    ;;                             x
+    ;;                             [2 4 3])))
+    ;;              ranges)
+        pitches (->> (apply concat ranges)
+                     (clojure.walk/postwalk
+                     (fn [x] (if (number? x) [x] x))))]
+    (->> (take-while (complement empty?)
+                     (tools/cyclic-partition
+                      pitches
+                      partitions))
+         (map (fn [x] (take-while (complement empty?)
+                                  (tools/cyclic-partition
+                                   x
+                                   [2 4 3])))))))
+
+(defn ascending
+  []
+  {:pitch (map (fn [x] [x])
+               (concat
+                (range 0 10)
+                (range 3 10)
+                (range 0 7)))
+   :part [:upper]
+   :fn tools/make-chord-from-pitch-vector-params
+   :partition #(tools/cyclic-partition % [1 1 1 2 2 1 1 2])
+   :duration [1/4 1/4 1/4 1/4]})
+
 (defn upper-soft
   []
   {:pitch (map (fn [x] [0 x])
@@ -15,4 +52,39 @@
    :duration [1/4 1/4 1/4 1/4]})
 
 (defn upper []
-  {:upper/a (tools/unfold-events (upper-soft))})
+  {:upper/b (tools/unfold-events (upper-soft))
+   :upper/a (tools/unfold-events (ascending))})
+
+;; (tools/cyclic-partition
+;;  (tools/cyclic-repeats
+;;   [0 2 3]
+;;   [5 2 3 4])
+;;  [3 4 2])
+
+;; if direction is down, set "step" to -x.
+;; partition subpartition
+
+
+;; (take 10 (tools/unfold-events (ascending)))
+(require '[melos.tools.schemata :as ms])
+(require '[schema.core :as s])
+
+(map (fn [x] (s/validate [ms/VerticalMoment] x))
+     (take 10 (tools/unfold-events (upper-soft))))
+
+(map (fn [x] (s/validate [ms/VerticalMoment] x))
+     (take 10 (tools/unfold-events (ascending))))
+
+(defn combine-partitions
+  [partitions]
+  (let [cycle-len (apply * (map (fn [x] (apply + x))
+                                partitions))
+        seqs (map (fn [part]
+                    (take-while (fn [x] (< x cycle-len))
+                                (reductions + 0 (cycle part))))
+                  partitions)]
+    (->> (sort (set (apply concat seqs)))
+         (partition 2 1)
+         (map (fn [[x y]] (- y x))))))
+
+(combine-partitions [[2 3 4] [7 8 9]])
