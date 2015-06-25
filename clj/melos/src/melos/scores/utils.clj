aee3d420ef9ae170323985ce1335fa66da5e7516
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-25 15:17:03 +0200

    Move all functions in scores/utils to tools/utils. Most of them are general enough to be used in other projects.

--- clj/melos/src/melos/scores/utils.clj
+++ clj/melos/src/melos/scores/utils.clj
@@ -2,60 +2,6 @@
   (:require [schema.core :as s]
             [melos.tools.schemata :as ms]
             [melos.tools.make-note :refer [make-note]]
-            [melos.tools.utils :refer [rotate]]))
+            [melos.tools.utils :refer [rotate maybe-vec make-chord-from-pitch-vector-params parse-params cyclic-partition cyclic-repeats combine-partitions unfold-events]]))
 
 
-(defn maybe-vec [x] (if (number? x) [x] x))
-
-(defn make-chord-from-pitch-vector-params
-  [{:keys [pitch] :as m}]
-  (let [group (gensym "G__")]
-  (map (fn [p]
-         (make-note (merge {:pitch p :group group}
-                           (dissoc m :pitch))))
-       pitch)))
-
-(defn parse-params
-  [x]
-  (if (list? x)
-    (repeat (last x) (first x))
-    [x]))
-
-(defn cyclic-partition
-  [splits xs]
-  (cons (take (first splits) xs)
-        (lazy-seq (cyclic-partition (rotate splits)
-                                    (drop (first splits) xs)))))
-
-(defn cyclic-repeats
-  [repeats xs]
-  (if (seq xs)
-    (concat (repeat (first repeats) (first xs))
-            (lazy-seq (cyclic-repeats (rotate repeats)
-                                      (rotate xs))))))
-
-(defn combine-partitions
-  [& partitions]
-  (let [cycle-len (apply * (map (fn [x] (apply + x))
-                                partitions))
-        seqs (map (fn [part]
-                    (take-while (fn [x] (< x cycle-len))
-                                (reductions + 0 (cycle part))))
-                  partitions)]
-    (->> (sort (set (apply concat seqs)))
-         (partition 2 1)
-         (map (fn [[x y]] (- y x))))))
-
-(defn unfold-events
-  [m]
-  (let [f (:fn m)
-        partition-fn (:partition m)
-        m (dissoc m :fn :partition)]
-  (->> (map (fn [x] x)
-            (vals m))
-       (map cycle)
-       (apply map vector)
-       (map (fn [x] (zipmap (keys m) x)))
-       (map f)
-       (partition-fn)
-       (map #(s/validate [ms/VerticalMoment] %)))))
--- clj/melos/src/melos/tools/utils.clj
+++ clj/melos/src/melos/tools/utils.clj
@@ -2,7 +2,10 @@
   (:require [clojure.java.io :as io]
             [clojure.walk :as walk]
             [schema.core :as s]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [schema.core :as s]
+            [melos.tools.schemata :as ms]
+            [melos.tools.make-note :refer [make-note]]))
 
 (defn abs [n] (max n (- n)))
 
@@ -68,3 +71,58 @@
 (defn mapply
   [f & args]
   (apply f (apply concat (butlast args) (last args))))
+
+(defn maybe-vec [x] (if (number? x) [x] x))
+
+(defn make-chord-from-pitch-vector-params
+  [{:keys [pitch] :as m}]
+  (let [group (gensym "G__")]
+  (map (fn [p]
+         (make-note (merge {:pitch p :group group}
+                           (dissoc m :pitch))))
+       pitch)))
+
+(defn parse-params
+  [x]
+  (if (list? x)
+    (repeat (last x) (first x))
+    [x]))
+
+(defn cyclic-partition
+  [splits xs]
+  (cons (take (first splits) xs)
+        (lazy-seq (cyclic-partition (rotate splits)
+                                    (drop (first splits) xs)))))
+
+(defn cyclic-repeats
+  [repeats xs]
+  (if (seq xs)
+    (concat (repeat (first repeats) (first xs))
+            (lazy-seq (cyclic-repeats (rotate repeats)
+                                      (rotate xs))))))
+
+(defn combine-partitions
+  [& partitions]
+  (let [cycle-len (apply * (map (fn [x] (apply + x))
+                                partitions))
+        seqs (map (fn [part]
+                    (take-while (fn [x] (< x cycle-len))
+                                (reductions + 0 (cycle part))))
+                  partitions)]
+    (->> (sort (set (apply concat seqs)))
+         (partition 2 1)
+         (map (fn [[x y]] (- y x))))))
+
+(defn unfold-events
+  [m]
+  (let [f (:fn m)
+        partition-fn (:partition m)
+        m (dissoc m :fn :partition)]
+  (->> (map (fn [x] x)
+            (vals m))
+       (map cycle)
+       (apply map vector)
+       (map (fn [x] (zipmap (keys m) x)))
+       (map f)
+       (partition-fn)
+       (map #(s/validate [ms/VerticalMoment] %)))))
