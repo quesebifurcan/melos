b388e6b416b6a210c61a0d82b86fef2f2fd43bcd
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-26 12:09:48 +0200

    Update build.sh -- remove local paths and clean up. Bypass the title page creation for now.

--- build.sh
+++ build.sh
@@ -1,8 +1,29 @@
-cd ./clj/melos
-lein exec -p src/melos/scores/test_event_seq.clj
-cd ../../
-ipython py/main.py ~/Desktop/score.json
-cd ./output
-pdflatex ../latex/title_page.tex
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=map.pdf ./title_page.pdf ./output.pdf
-open ./map.pdf
+#!/bin/bash
+
+set -e
+set -u
+
+TITLE=$1
+AUTHOR=$2
+
+REPO_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+CLJ_DIR=$REPO_DIR/clj/melos
+PY_DIR=$REPO_DIR/py
+OUTPUT_DIR=$REPO_DIR/output
+JSON_SCORE_FILE_NAME=$OUTPUT_DIR/output.json
+
+cd $CLJ_DIR
+lein run $JSON_SCORE_FILE_NAME
+
+cd $PY_DIR
+ipython main.py \
+        $JSON_SCORE_FILE_NAME \
+        $TITLE \
+        $AUTHOR \
+        $OUTPUT_DIR/output.pdf \
+        $OUTPUT_DIR/output.mid
+
+# TODO: Fix title page.
+# cd $OUTPUT_DIR
+# pdflatex ../latex/title_page.tex
+# gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=map.pdf ./title_page.pdf ./output.pdf
