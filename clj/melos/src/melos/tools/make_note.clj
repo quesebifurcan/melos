68e531947786d93eb7332659a4a7b44b6241cc38
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-26 17:35:02 +0200

    Remove unused code for onset- and contour-calculations.

--- clj/melos/src/melos/tools/make_note.clj
+++ clj/melos/src/melos/tools/make_note.clj
@@ -1,6 +1,4 @@
-(ns melos.tools.make-note
-  (:use [schema.core])
-  (:require [melos.tools.schemata :as ms]))
+(ns melos.tools.make-note)
 
 (defn note-default
   []
@@ -23,55 +21,3 @@
   "Make a representation of a musical note."
   [params]
   (merge (note-default) params))
-
-(defrecord Note
-    [pitch :- ms/Pitch
-     delta-dur :- Num
-     duration :- Num
-     dissonance-contributor? :- Bool
-     allow-extension? :- Bool
-     color :- Any
-     merge-left? :- Bool
-     merge-right? :- Bool
-     group :- Any
-     onset :- Num
-     is-rest? :- Bool
-     part :- ms/PartName
-     intensity :- Num
-     count :- Int])
-
-(defn make-note-
-  [{:keys [pitch
-           delta-dur
-           duration
-           dissonance-contributor?
-           allow-extension?
-           color
-           merge-left?
-           merge-right?
-           group
-           onset
-           is-rest?
-           part
-           intensity
-           count]}]
-  (validate Note
-            (Note. pitch
-                   delta-dur
-                   duration
-                   dissonance-contributor?
-                   allow-extension?
-                   color
-                   merge-left?
-                   merge-right?
-                   group
-                   onset
-                   is-rest?
-                   part
-                   intensity
-                   count)))
-
-;; (defn make-note
-;;   [params]
-;;   (make-note- (merge (note-default)
-;;                      params)))
