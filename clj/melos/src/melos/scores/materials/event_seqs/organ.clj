a0c52b800f6bc30d3d97f0621d2dade02525a25a
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-26 16:23:04 +0200

    Combine all event-seq-maps into an "organ" instrument.

---
+++ clj/melos/src/melos/scores/materials/event_seqs/organ.clj
@@ -0,0 +1,11 @@
+(ns melos.scores.materials.event-seqs.organ
+  (:require [melos.scores.materials.event-seqs.lower :refer [lower]]
+            [melos.scores.materials.event-seqs.ped :refer [ped]]
+            [melos.scores.materials.event-seqs.upper :refer [upper]]))
+
+(defn organ
+  []
+  (merge (upper)
+         (lower)
+         (ped)))
+
