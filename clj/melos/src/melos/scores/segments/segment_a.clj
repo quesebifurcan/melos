25085a3e76b75190bd14db79d269f7dbd3aefbc6
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-06-28 04:34:49 +0200

    Add a minimal segment-file which can be used as a template for new segments.

---
+++ clj/melos/src/melos/scores/segments/segment_a.clj
@@ -0,0 +1,36 @@
+(ns melos.scores.segments.segment-a
+  (:require [schema.core :as s]
+            [melos.tools.schemata :as ms]
+            [melos.tools.cycle-params :refer [unfold-parameter-cycles]]
+            [melos.tools.utils :as utils]
+            [melos.scores.compose-segment :refer [compose-segment]]
+            [melos.scores.graphs.score-graph :as score-graph]
+            [melos.scores.materials.event-seqs.organ :as organ]
+            [melos.scores.materials.measures :as measures]
+            [melos.scores.materials.dissonance-fn-params :as dissonance-fn-params]
+            [melos.scores.materials.dissonance-maps :as dissonance-maps]
+            [melos.scores.ctrl-fns.pairwise :as pairwise]))
+
+(s/defn initial-score-segment
+  :- ms/ScoreSegment
+  []
+  (utils/make-score-segment {:melodic-indices [:upper/a :lower/a :ped/a]
+                             :diss-fn-params (:a dissonance-fn-params/presets)
+                             :interval->diss-map (:default dissonance-maps/presets)
+                             :time-signatures [measures/measure-3]
+                             :mod-dur-patterns []
+                             :tempo 144
+                             :part-names [:upper :lower :ped]
+                             :melody-sources (atom (organ/organ))}))
+
+(s/defn changes
+  :- [ms/PartialScoreSegment]
+  []
+  (unfold-parameter-cycles [{}] 5))
+
+(defn compose
+  []
+  (compose-segment {:initial-state (initial-score-segment)
+                    :changes (changes)
+                    :graph score-graph/lazy-segment-graph}))
+
