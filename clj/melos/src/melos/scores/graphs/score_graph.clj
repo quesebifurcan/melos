f4f29ae3c0fd71c7cba9df81ab501b40fdeee826
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-08-04 08:07:17 +0200

    Update score_graph to work exclusively with lists (not lists *and* trees).

--- clj/melos/src/melos/scores/graphs/score_graph.clj
+++ clj/melos/src/melos/scores/graphs/score_graph.clj
@@ -2,16 +2,11 @@
     (:require [plumbing.graph :as graph]
               [plumbing.core :refer [fnk]]
               [melos.tools.selector-sequence :as sel-seq]
-              [melos.tools.modify-durations :as mod-dur]
               [melos.tools.default-horizontal-merge :as horizontal-merge]
               [melos.tools.dissonance-calculator :as diss-calc]
-              [melos.tools.filter-parts :as filter-parts]
-              [melos.tools.rtm :as rtm]
               [melos.tools.delay-lines :as delay-lines]))
 
-(require '[melos.tools.dissonance-calculator :refer [scaled-dissonance-value]])
-
-(def segment-graph
+(def score->event-seq
   {:events
    (fnk [melodic-indices melody-sources-atom]
         (sel-seq/collect-events-in-segment melodic-indices
@@ -33,50 +28,6 @@
         ((apply comp mod-dur-patterns) extended-events))
    :merged-horizontally
    (fnk [modified-durations]
-        (horizontal-merge/maybe-merge modified-durations))
-   :rhythmic-tree
-   (fnk [time-signatures merged-horizontally]
-        (rtm/make-r-tree merged-horizontally
-                         time-signatures))
-   :result
-   (fnk [tempo part-names rhythmic-tree]
-        {:tempo tempo
-         :parts (->> (map
-                      (fn [part-name]
-                        {:part-name part-name
-                         :events (filter-parts/split-out-part rhythmic-tree part-name)})
-                      part-names)
-                     (rtm/merge-all-tied))})})
-
-(def ranking-graph
-  {:extended-events
-   (fnk [events diss-fn-params interval->diss-map]
-        ;; Set dissonance-map (globally).
-        ;; TODO: more elegance.
-        (swap! diss-calc/dissonance-value
-               (fn [_]
-                 (diss-calc/dissonance-value-partial interval->diss-map)))
-        (let [fn_ (delay-lines/handle-dissonance diss-fn-params)]
-          (rest (reductions fn_ [] events))))
-   :modified-durations
-   (fnk [extended-events mod-dur-patterns]
-        ((apply comp mod-dur-patterns) extended-events))
-   :merged-horizontally
-   (fnk [modified-durations]
         (horizontal-merge/maybe-merge modified-durations))})
 
-   ;; :rhythmic-tree
-   ;; (fnk [time-signatures merged-horizontally]
-   ;;      (rtm/make-r-tree merged-horizontally
-   ;;                       time-signatures))
-   ;; :result
-   ;; (fnk [tempo part-names modified-durations rhythmic-tree]
-   ;;      {:tempo tempo
-   ;;       :parts (->> (map
-   ;;                    (fn [part-name]
-   ;;                      {:part-name part-name
-   ;;                       :events (filter-parts/split-out-part rhythmic-tree part-name)})
-   ;;                    part-names)
-   ;;                   (rtm/merge-all-tied))})})
-
-(def lazy-segment-graph (graph/lazy-compile segment-graph))
+(def lazy-segment-graph (graph/lazy-compile score->event-seq))
