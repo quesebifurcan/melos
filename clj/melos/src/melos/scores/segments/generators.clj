c06120e19143506d6711f40d8eed51ecb4b559c3
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-08-04 08:51:42 +0200

    Simplify composition of a segment -- use maps to control the rhythmic parameters.

--- clj/melos/src/melos/scores/segments/generators.clj
+++ clj/melos/src/melos/scores/segments/generators.clj
@@ -9,75 +9,8 @@
             [melos.scores.materials.measures :as measures]
             [melos.scores.materials.dissonance-maps :as dissonance-maps]
             [melos.scores.ctrl-fns.stepwise :as stepwise]
+            [melos.scores.segments.unfold-event-seq :as unfold-event-seq]
             [melos.scores.ctrl-fns.pairwise :as pairwise]))
-
-(s/defn initial-score-segment
-  :- ms/ScoreSegment
-  []
-  (utils/make-score-segment {:melodic-indices (take 20 (cycle [
-                                                               :upper/a
-                                                               :lower/a
-                                                               :ped/a
-                                               ]))
-                             :diss-fn-params {:max-count 10
-                                              :max-lingering 300
-                                              :diss-value [0 2 4 5]}
-                             :interval->diss-map dissonance-maps/default
-                             :time-signatures [measures/measure-4]
-                             ;; :mod-dur-patterns [pairwise/sustain-dissonant-melody]
-                             ;; :mod-dur-patterns [stepwise/remove-dissonant-vertical-moments]
-                             ;; :mod-dur-patterns [stepwise/dissonance->durations-mapping]
-                             ;; :mod-dur-patterns [pairwise/sustain-parts-count-drops]
-                             ;; :mod-dur-patterns [stepwise/apply-melodic-pitch-class-mapping]
-                             ;; :mod-dur-patterns [stepwise/apply-durations]
-                             :mod-dur-patterns [stepwise/sort-continuous]
-                             :tempo 240
-                             :part-names [:upper :lower :ped]
-                             :melody-sources (atom (event-seqs/organ))}))
-
-(defn melodic-indices
-  []
-  (let [cnts [1000 4000 4000 4000]]
-    (map (fn [cnt pattern]
-           ;; (take cnt (cycle [
-           ;;                   ;; :upper/a :lower/a :upper/a :ped/a :ped/a :lower/a
-           ;;                   ;; :upper/a :lower/a :upper/a :ped/a
-           ;;                   ;; :upper/a :lower/a :ped/a
-           ;;                   ;; :upper/a :lower/a :ped/a :ped/a
-           ;;                   ;; :upper/a :lower/a
-           ;;                   ;; :upper/a :lower/a
-           ;;                   ;; :upper/a :lower/a
-           ;;                   ])))
-           (take cnt (cycle pattern)))
-         cnts
-         [
-          [:upper/a :lower/a :ped/a]
-          [:upper/a :lower/a :upper/a :ped/a]
-          [:upper/a :lower/a :upper/a :lower/a :ped/a]
-          [:upper/a :lower/a :upper/a :ped/a :lower/a :ped/a]
-          ])))
-
-(s/defn changes
-  :- [ms/PartialScoreSegment]
-  []
-  (let [tempo-measure-link [2 2]]
-    (unfold-parameter-cycles
-     [{:values (melodic-indices)
-       :path [:melodic-indices]
-       :cycle [1]}
-      {:values [[measures/measure-4] [measures/measure-3]]
-       :path [:time-signatures]
-       :cycle tempo-measure-link}
-      {:values [240]
-       :path [:tempo]
-       :cycle tempo-measure-link}]
-     1)))
-
-(defn compose
-  []
-  (compose-segment {:initial-state (initial-score-segment)
-                    :changes (changes)
-                    :graph score-graph/lazy-segment-graph}))
 
 (require '[melos.tools.rtm :as rtm]
          '[melos.tools.filter-parts :as filter-parts]
@@ -92,87 +25,60 @@
   (map (fn [x] (zipmap (keys m) x))
        (apply combinatorics/cartesian-product (vals m))))
 
-(defn evaluate-nested-fns
-  [state]
-  (clojure.walk/postwalk
-   (fn [form]
-     (if (and (map? form)
-              (contains? form :fn)
-              (contains? form :params))
-       ((:fn form) (:params form))
-       form))
-   state))
-
-(defn evaluate-once
-  [state changes]
-  (let [state (reduce (fn [m [k v]]
-                        (update-in m k (fn [_] v)))
-                      state
-                      changes)]
-    (->> state
-         (evaluate-nested-fns)
-         (score-graph/lazy-segment-graph)
-         (:merged-horizontally)
-         )))
+(defn calc-event-combination
+  [state updates]
+  (->> (unfold-event-seq/unfold-events state updates)
+       (score-graph/lazy-segment-graph)
+       (:result)))
 
 (defn calc-event-combinations
   [state changes]
-  (map (partial evaluate-once state)
-       changes))
-
-(defn apply-rhythm
-  [last-event-extension tempo measures events]
-  (->> events
-       (rtm/extend-last last-event-extension)
-       (rtm/make-r-tree measures)
-       ((fn [rhythmic-tree]
-          {:tempo tempo
-           :parts (->> (map
-                        (fn [part-name]
-                          {:part-name part-name
-                           :events (filter-parts/split-out-part rhythmic-tree part-name)})
-                        [:upper :lower :ped])
-                       (rtm/merge-all-tied))}))))
+  (map (partial calc-event-combination state) changes))
 
 (defn upper-part
   [{:keys [transposition part-name]}]
   (utils/unfold-events (event-seqs/upper part-name transposition)))
 
-(let [changes (unfold-parameters
-               {[:melody-sources :upper/a :params :transposition] [-10 10 0]
-                [:melodic-indices] [
-                                    (take 21 (cycle [:upper/a :lower/a :ped/a]))
-                                    (take 18 (cycle [:upper/a :lower/a :upper/a :ped/a]))
-                                    ]
-                [:diss-fn-params :diss-value] [[0 2 4 5] [0 1 2]]
-                })
-      state {:melody-sources {
-                              :upper/a {:fn upper-part
-                                        :params {:transposition -3
-                                                 :part-name :upper}}
-                              :lower/a {:fn upper-part
-                                        :params {:transposition -5
-                                                 :part-name :lower}}
-                              :ped/a {:fn upper-part
-                                      :params {:transposition -15
-                                               :part-name :ped}}
-                              }
-             :diss-fn-params {:max-count 10
-                              :max-lingering 300
-                              :diss-value [0 2 4 5]}
-             :interval->diss-map dissonance-maps/default
-             :time-signatures [measures/measure-4]
-             :mod-dur-patterns []
-             :tempo 240
-             :part-names [:upper :lower :ped]
-             :melodic-indices []}
-      events (calc-event-combinations state changes)]
+(defn compose-all
+  [filters phrases]
+  (->> (filters phrases)
+       (map (fn [phrase] (-> (score-graph/lazy-rtm-graph {:tempo 240
+                                                          :event-seq phrase
+                                                          :measures [measures/measure-4]
+                                                          :part-names [:upper :lower :ped]
+                                                          :last-event-extension 7/4})
+                             (:result))))))
 
-  (->> (sort-by (fn [x]
-                  (apply + (map count x)))
-                events)
-       (map (partial apply-rhythm 7/4 240 [measures/measure-4]))
-       (utils/export-to-json
-        "/Users/fred/Desktop/score.json")
-       ))
-
+(defn compose
+  []
+  (let [changes (unfold-parameters
+                 {[:melody-sources :upper/a :params :transposition] [-10 10 0]
+                  [:melodic-indices] [
+                                      (take 21 (cycle [:upper/a :lower/a :ped/a]))
+                                      (take 18 (cycle [:upper/a :lower/a :upper/a :ped/a]))
+                                      ]
+                  [:diss-fn-params :diss-value] [[0 2 4 5] [0 1 2]]
+                  })
+        state {:melody-sources {
+                                :upper/a {:fn upper-part
+                                          :params {:transposition -3
+                                                   :part-name :upper}}
+                                :lower/a {:fn upper-part
+                                          :params {:transposition -5
+                                                   :part-name :lower}}
+                                :ped/a {:fn upper-part
+                                        :params {:transposition -15
+                                                 :part-name :ped}}
+                                }
+               :diss-fn-params {:max-count 10
+                                :max-lingering 300
+                                :diss-value [0 2 4 5]}
+               :interval->diss-map dissonance-maps/default
+               :time-signatures [measures/measure-4]
+               :mod-dur-patterns []
+               :tempo 240
+               :part-names [:upper :lower :ped]
+               :melodic-indices []}
+        filters #(sort-by (fn [x] (apply + (map count x))) %)]
+    (->> (calc-event-combinations state changes)
+         (compose-all filters))))
