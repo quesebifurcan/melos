2342b2e03f9dc6291bba40a385299c4eb76e70e5
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-08-09 17:07:08 +0200

    Annotate default-horizontal-merge.

--- clj/melos/src/melos/chord_seq/default_horizontal_merge.clj
+++ clj/melos/src/melos/chord_seq/default_horizontal_merge.clj
@@ -1,26 +1,34 @@
-(ns melos.chord-seq.default-horizontal-merge)
+(ns melos.chord-seq.default-horizontal-merge
+  (:require [schema.core :as s]
+            [melos.schemas.schemas :as ms]))
 
 ;; Merge horizontally
 
-(defn can-merge?
-  [curr next]
+(s/defn can-merge?
+  :- s/Bool
+  [curr :- ms/VerticalMoment
+   next :- ms/VerticalMoment]
   (let [old-curr (filter #(> (:count %) 0) next)
         news (filter #(= (:count %) 0) next)]
     (and (= (count curr) (count old-curr))
          (every? #(:merge-left? %) news)
          (every? #(:merge-right? %) old-curr))))
 
-(defn merge-elts
-  [a b]
+(s/defn merge-elts
+  :- ms/VerticalMoment
+  [a :- ms/VerticalMoment
+   b :- ms/VerticalMoment]
   (let [melodic-notes (filter #(= (:count %) 0) b)]
     (concat a melodic-notes)))
 
-(defn maybe-merge
-  ([events]
+(s/defn maybe-merge
+  :- [ms/VerticalMoment]
+  ([events :- [ms/VerticalMoment]]
    (if (seq events)
      (maybe-merge (first events)
                   (rest events))))
-  ([head events]
+  ([head :- ms/VerticalMoment
+    events :- [ms/VerticalMoment]]
    (cond (empty? events)
          (list head)
          (can-merge? head (first events))
