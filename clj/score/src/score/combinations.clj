06a2f81cbdee2085ba830f07c6ba6b2fe4be8fd0
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-08-10 08:37:27 +0200

    Add new function for "unfolding" tagged values in a nested data structure. Given e.g. a map, it produces as many new maps as it is needed to cover all possible permutations of the tagged values.

--- clj/score/src/score/combinations.clj
+++ clj/score/src/score/combinations.clj
@@ -6,31 +6,33 @@
   (map (fn [x] (zipmap (keys m) x))
        (apply combinatorics/cartesian-product (vals m))))
 
-(defn nested-map-product
-  [x]
-  (let [coll (atom {})
-        mod-form (clojure.walk/prewalk
-                  (fn [form]
-                    (if (and (vector? form)
-                             (= (first form) 'unfold))
-                      (let [placeholder (str (gensym "A__"))
-                            value (rest form)]
-                        (do (swap! coll assoc placeholder value)
-                            placeholder))
-                      form))
-                  x)]
-    (let [placeholders @coll]
-      (swap! coll unfold-parameters)
-      (map (fn [z]
-             (clojure.walk/prewalk
-              (fn [form]
-                (if (and (string? form)
-                         (contains? placeholders form))
-                  (get z form)
-                  form))
-              mod-form))
-           @coll))))
+(defn- substitute-tagged-elts
+  [tag placeholders form]
+  (if (and (vector? form)
+           (= (first form) tag))
+    (let [placeholder (str (gensym "A__"))
+          value (second form)]
+      (do (swap! placeholders assoc placeholder value)
+          placeholder))
+    form))
 
-(nested-map-product {:a '[unfold 1 2 3]
-                     :oij {:ui [98] :ahfd '[unfold 7 6 8 8 7 6 5 4 3 33 3 3]}
-                     :b {:testing {:a '[unfold 98 76 23]}}})
+(defn- replace-if-placeholder
+  [replacement-map form]
+  (if (and (string? form)
+           (contains? replacement-map form))
+    (get replacement-map form)
+    form))
+
+(defn nested-map-product
+  [form]
+  (let [placeholders (atom {})
+        mod-form (clojure.walk/prewalk
+                  (partial substitute-tagged-elts 'unfold placeholders)
+                  form)]
+    (swap! placeholders unfold-parameters)
+    (map (fn [replacement-map]
+           (clojure.walk/prewalk
+            (partial replace-if-placeholder replacement-map)
+            mod-form))
+         @placeholders)))
+
