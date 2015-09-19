807879c6a8a82b178e5437adb8ac426000bfdd8f
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-09-19 22:08:28 +0200

    refactor: split compose-segment in two parts, add partition- and filter-functions inbetween.

--- clj/score/src/score/compose_score.clj
+++ clj/score/src/score/compose_score.clj
@@ -12,7 +12,8 @@
             [schema.core :as s]
             [score.combinations :as combinations]
             [score.materials
-             [measures :as measures]]))
+             [measures :as measures]
+             [stepwise-mod :as stepwise-mod]]))
 
 ;;-----------------------------------------------------------------------------
 ;; SCORE
@@ -32,6 +33,27 @@
        ((apply comp post))
        (rhythm-tree/make-r-tree measures)
        (part/compose-part tempo part-names)))
+
+(defn compose-event-seq
+  [{:keys [events
+           diss-fn-params
+           pre
+           post]}]
+  (->> events
+       ((apply comp pre))
+       (chord-seq/extend-events diss-fn-params)
+       (chord-seq/merge-horizontally)
+       ((apply comp post))))
+
+(defn compose-parts
+  [measures tempo part-names event-seq]
+  (let [head (stepwise-mod/maybe-split-vertical-moment 
+              (first event-seq))
+        event-seq-mod (concat head (rest event-seq))]
+  (->> event-seq-mod
+       (rhythm-tree/extend-last 7/4)
+       (rhythm-tree/make-r-tree measures)
+       (part/compose-part tempo part-names))))
 
 (defn make-overlaps [pitches]
   (->> pitches
@@ -89,15 +111,34 @@
    :merge-right? [true]
    :duration [1/4]})
 
-(def chord-seq
-  (let [melodic-indices (take 50 (cycle [:upper/a :lower/a :ped/a]))
-        fn_ (comp utils/unfold-events diatonic-ped)
-        melody-sources-atom (atom {:lower/a (fn_ :lower -7)
-                                   :upper/a (utils/unfold-events (upper :upper -3))
-                                   ;; :upper/a (upper)
-                                   :ped/a (utils/unfold-events (diatonic-ped-2 :ped -20))})]
+(def materials
+  {:upper [
+           (utils/unfold-events (upper :upper -3))
+           (utils/unfold-events (upper :upper 3))
+           ]
+   :lower [
+           (utils/unfold-events (diatonic-ped :lower -7))
+           (utils/unfold-events (diatonic-ped :lower -5))
+           ]
+   :ped [
+         (utils/unfold-events (diatonic-ped-2 :ped -20))
+         (utils/unfold-events (diatonic-ped-2 :ped -10))
+         ]
+   :melodic-indices [(take 50 (cycle [:upper :lower :ped]))]
+   })
+
+(defn make-chord-seq
+  [{:keys [upper lower ped melodic-indices]}]
+  (let [melody-sources-atom (atom {:lower lower
+                                   :upper upper
+                                   :ped ped})]
     (chord-seq/collect-events-in-segment melodic-indices
-                               melody-sources-atom)))
+                                         melody-sources-atom)))
+
+(def chord-seqs
+  (->> materials
+       (combinations/unfold-parameters)
+       (map make-chord-seq)))
 
 (def diss-params
   {:check (fn [events]
@@ -105,8 +146,8 @@
                 (chord/scaled-dissonance-value [0 2 4 5])))})
 
 (defn initial-state
-  []
-  {:events chord-seq
+  [events]
+  {:events events
    :diss-fn-params {:max-count 100
                     :max-lingering 300
                     ;; TODO: pass on diss-value
@@ -114,12 +155,54 @@
    :tempo 120
    :measures [measures/measure-4]
    :pre []
-   ;; :post [(partial rtm/extend-last 7/4)]
+   ;; :post [(partial rhythm-tree/extend-last 7/4)]
    :post []
    :part-names [:upper :lower :ped]})
 
-(defn compose []
-  (->> (compose-segment
-        (initial-state))
-       (conj [])))
+(defn parts-in-chord
+  [chord]
+  (set (map :part chord)))
 
+(defn part-count-sufficient?
+  [minimum chord]
+  (let [part-count ((comp count parts-in-chord) chord)]
+    (= part-count minimum)))
+
+(def partition-events-fn
+  (partial part-count-sufficient? 3))
+
+(defn filter-events-fn
+  [events]
+  (and (>= (count events) 5)
+       (every? (partial part-count-sufficient? 3) events)))
+
+(defn compose []
+  (let [states (map initial-state chord-seqs)]
+  (->> states
+       (map compose-event-seq)
+       ;; (first)
+       ;; (map (partial compose-parts
+       ;;               [measures/measure-4]
+       ;;               120
+       ;;               [:upper :lower :ped])))))
+       ;; (partition-by
+       ;;  (partial part-count-sufficient? 2))
+       (apply concat)
+       (partition-by partition-events-fn)
+       (filter filter-events-fn)
+       ;; (apply concat)
+       (map (partial compose-parts
+                     [measures/measure-4]
+                     120
+                     [:upper :lower :ped]))
+       ;; (map (fn [x]
+       ;;        (map (comp count parts-in-chord) x)))
+       ;; (map (fn [x] (count x)))
+       )))
+
+;; measures tempo part-names
+
+(compose)
+
+;; (partition-by #(>= % 3)
+;;               [0 1 3 3 2 4 3 3 3 1 2 4 2 3 3 3 2])
