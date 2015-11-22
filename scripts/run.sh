b3d6c8e099d095e8cd923eaf4d1854644a375f0c
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-11-22 17:00:40 +0100

    Move all score-related config data to .edn.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,13 @@
 set -e
 
+if [ -z "$1" ]
+then
+    echo "No config file specified; exiting."
+    exit
+else
+    SCORE_CONFIG=$1
+fi
+
 red=$(tput setaf 1)
 normal=$(tput sgr0)
 
@@ -8,6 +16,7 @@ CLOJURE_DIR=$REPO_DIR/clj/melos
 
 source $REPO_DIR/scripts/defaults.cfg
 mkdir -p $ANALYSIS_DIR
+mkdir -p $OUTPUT_DIR
 
 # Run in separate terminal window:
 # lein trampoline repl :headless
@@ -15,28 +24,16 @@ mkdir -p $ANALYSIS_DIR
 cd $CLOJURE_DIR
 
 echo "${red}\nReloading namespaces...${normal}"
+
 grench eval "(require '[clojure.tools.namespace.repl :refer [refresh]]) (refresh)"
+grench eval "(time (melos.score.main/-main \"$JSON_OUTPUT\" \"$ANALYSIS_DIR\"  \"$1\"))"
 
-if [ "$RECALCULATE_SESSIONS" = true ]; then
-    echo "${red}\nCalculating sessions...${normal}"
-    for session in ${SESSIONS[@]}
-    do
-        grench eval "(time (melos.score.compose-score/new-session $session))"
-    done
-fi
+cd $REPO_DIR
 
-if [ "$RENDER_SCORE" = true ]; then
-    echo "${red}\nComposing score...${normal}"
-    grench eval "(time (melos.score.main/-main \"$JSON_OUTPUT\" $COMPOSITION_FN [$SESSIONS]))"
-
-    cd $REPO_DIR
-
-    echo "${red}\nABJAD...${normal}"
-    source $PY_VIRTUALENV && \
-        ipython py/main.py \
-                $JSON_OUTPUT \
-                $TITLE \
-                $AUTHOR \
-                $PDF_OUTPUT \
-                $MIDI_OUTPUT
-fi
+echo "${red}\nABJAD...${normal}"
+source $PY_VIRTUALENV && \
+    ipython py/main.py \
+            $SCORE_CONFIG \
+            $JSON_OUTPUT \
+            $PDF_OUTPUT \
+            $MIDI_OUTPUT
