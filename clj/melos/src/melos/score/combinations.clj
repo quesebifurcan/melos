aa3ffcb610f0913a486f377bd2c41b2f0bcee6cc
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2015-11-29 20:08:41 +0100

    Add `weave-seqs` -- a variant of `interleave`, with which sequences of different lengths can be interwoven.

--- clj/melos/src/melos/score/combinations.clj
+++ clj/melos/src/melos/score/combinations.clj
@@ -36,3 +36,48 @@
             mod-form))
          @placeholders)))
 
+;; weave-seqs helper functions
+
+(defn- get-highest-keys
+  [m]
+  (let [max-val (apply max (vals m))]
+    (->> m
+         (filter (fn [[k v]] (= v max-val)))
+         (keys))))
+
+(defn- get-next-key
+  [m exclude]
+  (let [candidates (set (get-highest-keys m))]
+    (if (> (count candidates) 1)
+      (first (clojure.set/difference candidates exclude))
+      (first candidates))))
+
+(defn- count-map->key-sequence
+  ([m]
+   (count-map->key-sequence m #{} []))
+  ([m prev coll]
+   (if (every? (fn [x] (= 0 x)) (vals m))
+     coll
+     (let [nxt (get-next-key m prev)]
+       (count-map->key-sequence (update m nxt dec)
+                                #{}
+                                (conj coll nxt))))))
+
+(defn- take-by-keyword-seq
+  [m kw-seq]
+  (if (empty? kw-seq)
+    nil
+    (let [curr-key (first kw-seq)]
+      (cons (first (curr-key m))
+            (take-by-keyword-seq (update m curr-key (partial drop 1))
+                                 (rest kw-seq))))))
+
+(defn weave-seqs
+  [m]
+  (let [count-map (clojure.algo.generic.functor/fmap count m)
+        key-seq (count-map->key-sequence count-map)]
+    (take-by-keyword-seq m key-seq)))
+
+;; (let [a {:a (range 7) :b (range 70 72) :c (range 1111 1122)}]
+;;   (weave-seqs a))
+
