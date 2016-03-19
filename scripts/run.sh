3988bc1153f59467035475a95979698d5cbd3c8a
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2016-03-19 18:39:38 +0100

    control: update main script.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,40 +1,37 @@
 set -e
 
-red=$(tput setaf 1)
-normal=$(tput sgr0)
-
-REPO_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ./../ && pwd)
-CLOJURE_DIR=$REPO_DIR/clj/melos
-
-source $REPO_DIR/scripts/defaults.cfg
-mkdir -p $ANALYSIS_DIR
-mkdir -p $OUTPUT_DIR
-
 # Run in separate terminal window:
 # lein trampoline repl :headless
 
+PY_VIRTUALENV=./env/bin/activate
+REPO_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ./../ && pwd)
+OUTPUT_DIR=$REPO_DIR/output
+CLOJURE_DIR=$REPO_DIR/clj/melos
+JSON_OUTPUT=$OUTPUT_DIR/score.json
+PDF_OUTPUT=$OUTPUT_DIR/score.pdf
+MIDI_OUTPUT=$OUTPUT_DIR/score.midi
+
+green=$(tput setaf 2)
+normal=$(tput sgr0)
+
+mkdir -p $OUTPUT_DIR
+
 cd $CLOJURE_DIR
-
-echo "${red}\nReloading namespaces...${normal}"
-
+echo "${green}Reloading namespaces...${normal}"
 SECTION_1=$REPO_DIR/output/section_1.json
-SECTION_2=$REPO_DIR/output/section_2.json
-
 grench eval "(require '[clojure.tools.namespace.repl :refer [refresh]]) (refresh)"
-grench eval "(time (melos.score.main/open \"$SECTION_1\"))"
-grench eval "(time (melos.score.main/closed \"$SECTION_2\"))"
+grench eval "(time (melos.score.main/make-score \"$SECTION_1\"))"
 
 cd $REPO_DIR
 
-echo "${red}\nABJAD...${normal}"
+echo "${green}Building score with Abjad...${normal}"
 source $PY_VIRTUALENV && \
     ipython py/main.py -- \
-           --title "Test" \
-           --author "Anonymous" \
-           --score-out $PDF_OUTPUT \
-           --midi-out $MIDI_OUTPUT \
-           --input-files \
-           $SECTION_1 \
-           $SECTION_2
+            --title "Test" \
+            --author "Anonymous" \
+            --score-out $PDF_OUTPUT \
+            --midi-out $MIDI_OUTPUT \
+            --input-files \
+            $SECTION_1
 
-open $PDF_OUTPUT
+echo "${green}Generated score in $PDF_OUTPUT${normal}"
