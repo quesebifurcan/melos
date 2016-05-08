ddb2451f3dbaf3126d0a7fdb4336692968bb2a0d
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2016-05-08 16:16:08 +0200

    refactor: move Note utility functions to note.clj

--- clj/melos/src/melos/lib/note.clj
+++ clj/melos/src/melos/lib/note.clj
@@ -1,3 +1,24 @@
 (ns melos.lib.note
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s]
+            [melos.lib.schemas :as ms])
+  (:import [melos.lib.schemas Note]))
+
+(defn note-default
+  []
+  {:count 0,
+   :dissonance-contributor? true
+   :group (gensym "G__")
+   :is-rest? false
+   :max-count 100
+   :merge-left? false
+   :merge-right? false
+   :notation nil
+   :part :none,
+   :pitch 0})
+
+(s/defn make-note
+  :- Note
+  [params :- s/Any]
+  (ms/map->Note (merge (note-default) params)))
+
 
