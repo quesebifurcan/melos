0a7f3fd3cd1a7cedca409628bb902bcbdd42e421
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2016-05-14 22:07:48 +0200

    utils: partition-by-inclusive

--- clj/melos/src/melos/lib/utils.clj
+++ clj/melos/src/melos/lib/utils.clj
@@ -132,6 +132,21 @@
                           coll
                           (rest l))))))
 
+(defn partition-by-inclusive
+  "like partition-by, but also puts the first non-matching element
+  in the split, and only groups results that return true in the pred f
+
+  Taken from here: https://github.com/mgaare/clojure-polyline/blob/master/src/clojure-polyline/core.clj
+  "
+  [f coll]
+  (lazy-seq
+   (when-let [s (seq coll)]
+     (let [run (take-while #(f %) s)
+           rem (seq (drop (count run) s))
+           included (first rem)
+           run-inc (concat run (vector included))]
+       (cons run-inc (partition-by-inclusive f (rest rem)))))))
+
 (defn unfold-parameters
   [m]
   (->> m
