d15ed42d23bae3547eb3bab429a878595554bb8d
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2016-10-03 10:04:25 +0200

    layout: add titlepage template

--- scripts/to_pdf.sh
+++ scripts/to_pdf.sh
@@ -4,7 +4,16 @@ cd $SCRIPT_DIR/..
 
 source $SCRIPT_DIR/env/bin/activate
 
+rm $SCRIPT_DIR/../output/example_1.ly
+rm $SCRIPT_DIR/../output/example_1.pdf
+
 python $SCRIPT_DIR/run.py \
        --input $SCRIPT_DIR/../$1 \
        --output $SCRIPT_DIR/../output/example_1.pdf
+
+pdfunite $SCRIPT_DIR/../output/titlepage.pdf \
+         $SCRIPT_DIR/../output/example_1.pdf \
+         $SCRIPT_DIR/../output/score.pdf
+
+open $SCRIPT_DIR/../output/score.pdf
 
